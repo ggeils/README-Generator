77ed1a13ae1cee0f4f9dfcb4885ca45503addbc2
George Geils <95538913+ggeils@users.noreply.github.com>
2022-01-26 22:22:51 -0500

    finished project, added comments, fixed indentation

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,21 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+// Loading required packages.
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateREADME = ({ title, description, installation, usage, license, 
-    contributing, github, email, tests, questions }) =>
+
+// Markdown to be generated.
+const generateREADME = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributing,
+  github,
+  email,
+  tests,
+  questions,
+}) =>
   `# ${title}
 
 ## Table of Contents
@@ -66,64 +79,67 @@ const generateREADME = ({ title, description, installation, usage, license,
  [Email Me](mailto:${email})
   `;
 
+
+// Input list with questions
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'What is the description of your project?',
+      type: "input",
+      name: "description",
+      message: "What is the description of your project?",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are your installation instructions?',
+      type: "input",
+      name: "installation",
+      message: "What are your installation instructions?",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'What is the usage of your project?',
+      type: "input",
+      name: "usage",
+      message: "What is the usage of your project?",
     },
     {
-      type: 'input',
-      name: 'license',
-      message: 'What license does your project have?',
+      type: "input",
+      name: "license",
+      message: "What license does your project have?",
     },
     {
-      type: 'input',
-      name: 'contributing',
-      message: 'Who contributed to the project?',
+      type: "input",
+      name: "contributing",
+      message: "Who contributed to the project?",
     },
     {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
+      type: "input",
+      name: "github",
+      message: "What is your github username?",
     },
     {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
+      type: "input",
+      name: "email",
+      message: "What is your email?",
     },
     {
-        type: 'input',
-        name: 'tests',
-        message: 'What are your test instructions?',
+      type: "input",
+      name: "tests",
+      message: "What are your test instructions?",
     },
     {
-        type: 'input',
-        name: 'questions',
-        message: 'Peviously asked questions?',
+      type: "input",
+      name: "questions",
+      message: "Peviously asked questions?",
     },
   ])
 
+  // Writing the README file.
   .then((answers) => {
     const readmeContent = generateREADME(answers);
 
-    fs.writeFile('README.md', readmeContent, (err) =>
-      err ? console.log(err) : console.log('Successfully generated README.md!')
+    fs.writeFile("README.md", readmeContent, (err) =>
+      err ? console.log(err) : console.log("Successfully generated README.md!")
     );
   });
