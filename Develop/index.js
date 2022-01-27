f988a668317eb2b0a8cd777491aadcf339d36154
George Geils <95538913+ggeils@users.noreply.github.com>
2022-01-26 21:40:09 -0500

    added various input questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,57 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({ title, description, installation, usage, license, 
+    contributing, github, email, tests, questions }) =>
+  ``;
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are your installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage of your project?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'What license does your project have?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Who contributed to the project?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateREADME(answers);
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
